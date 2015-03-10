8e272f224c5de244b02fdebc191f07883b183ca6
Terrel Shumway <terrel@gmail.com>
2015-03-10 08:30:00 -0600

    add AnonymityPoll dataset

---
+++ week1/privacy.R
@@ -0,0 +1,22 @@
+
+baseurl = "https://courses.edx.org/c4x/MITx/15.071x_2/asset/"
+files = c( "AnonymityPoll.csv")
+
+
+get_data = function(url,local){
+  if(!file.exists(local)){
+    download.file(url,local,"curl")
+  }
+  read.csv(local)
+}
+
+data_dir = function(fname){
+  paste("data",fname,sep="/")
+}
+
+getm = function(file){
+  get_data(paste(baseurl,file,sep=""),data_dir(file))
+}
+
+ipd=getm(files)
+
