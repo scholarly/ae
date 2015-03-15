f63070ff3488b26369558bfb1713c45ebb47502c
Terrel Shumway <terrel@gmail.com>
2015-03-14 23:35:27 -0600

    work on readingScores problems

---
+++ week2/literate.R
@@ -0,0 +1,44 @@
+baseurl = "https://courses.edx.org/c4x/MITx/15.071x_2/asset/"
+files = c( "pisa2009train.csv","pisa2009test.csv")
+
+
+get_data = function(url,local){
+  if(!file.exists(local)){
+    download.file(url,local,"curl")
+  }
+  read.csv(local)
+}
+
+data_dir = function(fname){
+  paste("data",fname,sep="/")
+}
+
+getm = function(file){
+  get_data(paste(baseurl,file,sep=""),data_dir(file))
+}
+
+source("lib/list.R")
+
+list[train,test] = lapply(files,getm)
+
+train = na.omit(train)
+test = na.omit(test)
+train$raceeth = relevel(train$raceeth,"White")
+test$raceeth = relevel(test$raceeth,"White")
+
+m1 = lm( readingScore ~ . ,data=train)
+
+m2 = step(m1)
+
+rmse = function(res) {sqrt(mean(res^2))}
+rsq = function(pred,act,mu) {1 - (sum((act-pred)^2)/sum((act-mu)^2))}
+
+
+p1 = predict(m1,test)
+p2 = predict(m2,test)
+
+mu = mean(train$Temp)
+
+print(rsq(p1,test$Temp,mu))
+print(rsq(p2,test$Temp,mu))
+print(rsq(p3,test$Temp,mu))
